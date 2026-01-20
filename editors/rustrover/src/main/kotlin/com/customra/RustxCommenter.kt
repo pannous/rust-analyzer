614675a8437d0a353c06fda222711cf680b217c7
pannous <info@pannous.com>
2026-01-20 13:48:23 +0100

    feature(minor): Add comment toggle support for .rust files in RustRover
    Add RustxCommenter class implementing Commenter interface with
    line comments (//) and block comments (/* */).

---
+++ editors/rustrover/src/main/kotlin/com/customra/RustxCommenter.kt
@@ -0,0 +1,11 @@
+package com.customra
+
+import com.intellij.lang.Commenter
+
+class RustxCommenter : Commenter {
+    override fun getLineCommentPrefix(): String = "//"
+    override fun getBlockCommentPrefix(): String = "/*"
+    override fun getBlockCommentSuffix(): String = "*/"
+    override fun getCommentedBlockCommentPrefix(): String? = null
+    override fun getCommentedBlockCommentSuffix(): String? = null
+}
