ec1a2a4f205d0e9e01ec639fe76d0666e7dd8079
pannous <info@pannous.com>
2026-01-20 10:45:24 +0100

    feature(minor): Add .rust extension support and crab file icon
    - Register .rust and .rs extensions with custom syntax highlighter
    - Add LSP mapping for .rust files
    - Add crab icon for Rustx file types

--- editors/rustrover/src/main/kotlin/com/customra/RustxFileType.kt
+++ editors/rustrover/src/main/kotlin/com/customra/RustxFileType.kt
@@ -1,11 +1,14 @@
 package com.customra
 
 import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 object RustxFileType : LanguageFileType(RustxLanguage) {
+    private val ICON = IconLoader.getIcon("/icons/rust.svg", RustxFileType::class.java)
+
     override fun getName() = "Rustx"
     override fun getDescription() = "Rust with language extensions"
     override fun getDefaultExtension() = "rx"
-    override fun getIcon(): Icon? = null
+    override fun getIcon(): Icon = ICON
 }
